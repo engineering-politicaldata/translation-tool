33faa476689e91da462445cc7f35f32ec4c05e99
Nikola_Shrutika <nikola_shrutika@Nikola-Shrutikas-MacBook-Pro.local>
2021-10-21 20:03:08 +0530

    add backend logic for project already exists

--- lib/backend.constants.ts
+++ lib/backend.constants.ts
@@ -1,3 +1,4 @@
 export enum ErrorCodes {
     'RESOURCE_ALREADY_EXITS' = 'RESOURCE_ALREADY_EXITS',
+    'PROJECT_ALREADY_EXISTS' = 'A Project with the given name already exists',
 }
--- pages/api/project/create.ts
+++ pages/api/project/create.ts
@@ -1,12 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { corsForPost } from '../../../lib/backend.config';
-import { CustomErrorHandler } from '../../../lib/backend.utils';
+import { CustomErrorHandler, CustomException } from '../../../lib/backend.utils';
+import { ErrorCodes } from '../../../lib/backend.constants';
 import DataProvider, { DataClient } from '../../../lib/data/DataProvider';
 import { CreateProjectInput } from '../../../lib/model';
 import { runMiddleware } from '../../../lib/run-middleware';
 
 async function createProjectWithDetails(input: CreateProjectInput) {
     const data: DataClient = await DataProvider.client();
+
+    const projectAlreadyExists = await data.pg
+        .select('id')
+        .from('project')
+        .where({
+            name: input.name,
+        })
+        .first();
+
+    if (projectAlreadyExists) {
+        throw new CustomException('Project Already Exits', ErrorCodes.PROJECT_ALREADY_EXISTS);
+    }
+
     let projectId = null;
     await data.pg.transaction(async trx => {
         try {
