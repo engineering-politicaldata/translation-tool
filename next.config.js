60d63323bfd2043a0de4c18ebad42fcece5f758e
Nikhil Chougule <nikhil.chougule@votercircle.com>
2021-10-21 07:51:00 +0530

    next config for env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 module.exports = {
     target: 'serverless',
     env: {
+        DB_SCHEMA: process.env.DB_SCHEMA,
+        DB_NAME: process.env.DB_NAME,
+        DB_USER: process.env.DB_USER,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_HOST: process.env.DB_HOST,
+        DB_PORT: process.env.DB_PORT,
+        DATABASE_POOL_MIN: process.env.DATABASE_POOL_MIN,
+        DATABASE_POOL_MAX: process.env.DATABASE_POOL_MAX,
+        DATABASE_POOL_IDLE: process.env.DATABASE_POOL_IDLE,
         NEXT_PUBLIC_IMG_URL_PREFIX: process.env.NEXT_PUBLIC_IMG_URL_PREFIX,
     },
     async redirects() {
