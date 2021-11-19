1e6ba5910debba3ebaa3ae0dd114f5e39546a26e
Shrutika <shrutika@politicaldata.com>
2021-11-19 14:50:38 +0530

    TT-51 | move resource-list in common component

---
+++ components/resources/resource-list.tsx
@@ -0,0 +1,99 @@
+import { Chip, LinearProgress, Typography, useTheme } from '@material-ui/core';
+import styled, { css } from 'styled-components';
+import { useRouter } from 'next/router';
+
+const ResourceListItemView = styled.div`
+    ${props =>
+        props.theme &&
+        css`
+            cursor: pointer;
+            padding: ${props.theme.spacing(3)}px ${props.theme.spacing(1)}px;
+            &:hover {
+                background-color: ${props.theme.grey[200]};
+            }
+            .chip-root {
+                height: auto;
+                margin-right: ${props.theme.spacing(2)}px;
+            }
+            .resource-item-stats {
+                display: flex;
+                align-items: center;
+                color: ${props.theme.grey[500]};
+                margin-bottom: ${props.theme.spacing(1)}px;
+
+                .section-1 {
+                    flex: 1;
+                    .source-name {
+                    }
+                }
+                .section-2 {
+                    margin-right: ${props.theme.spacing(4)}px;
+                }
+                .section-3 {
+                    display: grid;
+                    grid-gap: ${props.theme.spacing(2)}px;
+                    text-align: center;
+                    padding: 0 ${props.theme.spacing(2)}px;
+                }
+            }
+        `}
+`;
+
+export const ResourceList = (props: any) => {
+    const theme = useTheme();
+    const router = useRouter();
+    const { resource, translationPercentage } = props;
+
+    const handleResourceItemClick = (resourceId: string) => {
+        const projectId = router.query.projectId;
+        const hrefUrl = '/project/[projectId]/resources/[resourceId]';
+        const asUrl = `/project/${projectId}/resources/${resourceId}`;
+        router.push(hrefUrl, asUrl);
+    };
+
+    return (
+        <ResourceListItemView
+            key={resource.id}
+            theme={theme}
+            onClick={() => handleResourceItemClick(resource.id)}
+        >
+            <div className='resource-item-stats'>
+                <div className='section-1'>
+                    <Typography variant='subtitle1'>
+                        <Chip
+                            label='Source'
+                            variant='outlined'
+                            className='chip-root'
+                            color='secondary'
+                            size='small'
+                        />
+                        {resource.sourceName}
+                    </Typography>
+                    <Typography variant='caption' color='inherit'>
+                        {Math.floor(translationPercentage)}% translated{' '}
+                    </Typography>
+                </div>
+                <div className='section-2'>
+                    <Typography variant='subtitle2' color='inherit'>
+                        {' '}
+                        {resource.totalSourceKeys} keys
+                    </Typography>
+                </div>
+                <div className='section-3'>
+                    <Typography variant='caption' color='inherit'>
+                        Updated On:
+                    </Typography>
+                    <Typography variant='caption' color='textPrimary'>
+                        {new Date(resource.created).toLocaleString()}
+                    </Typography>
+                </div>
+            </div>
+            <LinearProgress
+                style={{ height: '8px' }}
+                variant='determinate'
+                color='secondary'
+                value={translationPercentage}
+            />
+        </ResourceListItemView>
+    );
+};
--- pages/project/[projectId]/resources/index.tsx
+++ pages/project/[projectId]/resources/index.tsx
@@ -11,6 +11,7 @@ import { UploadResourceInput } from '@data-model';
 import { GET_API_CONFIG, POST_API_CONFIG } from '../../../../shared/ApiConfig';
 import { APPBAR_HEIGHT, LoadingState } from '../../../../shared/Constants';
 import { apiRequest } from '../../../../shared/RequestHandler';
+import { ResourceList } from '../../../../components/resources/resource-list';
 
 const ProjectResourcesPage = styled.div`
     ${props =>
@@ -94,42 +95,6 @@ const ProjectResourcesPage = styled.div`
         `}
 `;
 
-const ResourceListItemView = styled.div`
-    ${props =>
-        props.theme &&
-        css`
-            cursor: pointer;
-            padding: ${props.theme.spacing(3)}px ${props.theme.spacing(1)}px;
-            &:hover {
-                background-color: ${props.theme.grey[200]};
-            }
-            .chip-root {
-                height: auto;
-                margin-right: ${props.theme.spacing(2)}px;
-            }
-            .resource-item-stats {
-                display: flex;
-                align-items: center;
-                color: ${props.theme.grey[500]};
-                margin-bottom: ${props.theme.spacing(1)}px;
-
-                .section-1 {
-                    flex: 1;
-                    .source-name {
-                    }
-                }
-                .section-2 {
-                    margin-right: ${props.theme.spacing(4)}px;
-                }
-                .section-3 {
-                    display: grid;
-                    grid-gap: ${props.theme.spacing(2)}px;
-                    text-align: center;
-                    padding: 0 ${props.theme.spacing(2)}px;
-                }
-            }
-        `}
-`;
 function ResourcesPage() {
     const router = useRouter();
     const projectId = router.query.projectId;
@@ -275,12 +240,6 @@ function ResourcesPage() {
         }
     }
 
-    const handleResourceItemClick = (resourceId: string) => {
-        const hrefUrl = '/project/[projectId]/resources/[resourceId]';
-        const asUrl = `/project/${projectId}/resources/${resourceId}`;
-        router.push(hrefUrl, asUrl);
-    };
-
     const UploadSourceButtom = (props: { title: string }) => {
         return (
             <div className='upload-file-button'>
@@ -317,49 +276,7 @@ function ResourcesPage() {
             const translationPercentage =
                 (resource.translatedKeysCount / resource.totalSourceKeys) * 100;
             return (
-                <ResourceListItemView
-                    key={resource.id}
-                    theme={theme}
-                    onClick={() => handleResourceItemClick(resource.id)}
-                >
-                    <div className='resource-item-stats'>
-                        <div className='section-1'>
-                            <Typography variant='subtitle1'>
-                                <Chip
-                                    label='Source'
-                                    variant='outlined'
-                                    className='chip-root'
-                                    color='secondary'
-                                    size='small'
-                                />
-                                {resource.sourceName}
-                            </Typography>
-                            <Typography variant='caption' color='inherit'>
-                                {Math.floor(translationPercentage)}% translated{' '}
-                            </Typography>
-                        </div>
-                        <div className='section-2'>
-                            <Typography variant='subtitle2' color='inherit'>
-                                {' '}
-                                {resource.totalSourceKeys} keys
-                            </Typography>
-                        </div>
-                        <div className='section-3'>
-                            <Typography variant='caption' color='inherit'>
-                                Updated On:
-                            </Typography>
-                            <Typography variant='caption' color='textPrimary'>
-                                {new Date(resource.created).toLocaleString()}
-                            </Typography>
-                        </div>
-                    </div>
-                    <LinearProgress
-                        style={{ height: '8px' }}
-                        variant='determinate'
-                        color='secondary'
-                        value={translationPercentage}
-                    />
-                </ResourceListItemView>
+                <ResourceList resource={resource} translationPercentage={translationPercentage} />
             );
         });
 
